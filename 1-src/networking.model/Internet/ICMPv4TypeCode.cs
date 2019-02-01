1ced850df42cdac9a3046b28e3ec4e7afd034468
lnh <lnhcode@outlook.com>
2019-02-01 10:23:59 +0800

    Add ICMPv4TypeCode Destination unreachable

--- 1-src/networking.model/Internet/ICMPv4TypeCode.cs
+++ 1-src/networking.model/Internet/ICMPv4TypeCode.cs
@@ -4,9 +4,13 @@ namespace Networking.Model.Internet
 {
     /// <summary>
     /// ICMPv4类型编号
+    /// <see href="https://en.wikipedia.org/wiki/internet_control_message_protocol#datagram_structure"/>
     /// </summary>
     public enum ICMPv4TypeCode : UInt16
     {
+
+        #region Echo
+
         /// <summary>
         /// Echo Request
         /// </summary>
@@ -16,5 +20,92 @@ namespace Networking.Model.Internet
         /// Echo Response
         /// </summary>
         EchoResponse = 0x0000,
+
+        #endregion
+
+
+        #region Destination unreachable
+
+        /// <summary>
+        /// Destination network unreachable
+        /// </summary>
+        DestinationUnreachableOfNetwork = 0x0300,
+
+        /// <summary>
+        /// Destination host unreachable
+        /// </summary>
+        DestinationUnreachableOfHost = 0x0301,
+
+        /// <summary>
+        /// Destination protocol unreachable
+        /// </summary>
+        DestinationUnreachableOfProtocol = 0x0302,
+
+        /// <summary>
+        /// Destination port unreachable
+        /// </summary>
+        DestinationUnreachableOfPort = 0x0303,
+
+        /// <summary>
+        /// Fragmentation required, and DF flag set 
+        /// </summary>
+        DestinationUnreachableOfFragmentationRequired = 0x0304,
+
+        /// <summary>
+        /// Source route failed 
+        /// </summary>
+        DestinationUnreachableOfSourceRouteFailed = 0x0305,
+
+        /// <summary>
+        /// Destination network unknown
+        /// </summary>
+        DestinationUnreachableOfNetworkUnknown  = 0x0306,
+
+        /// <summary>
+        /// Destination host unknown
+        /// </summary>
+        DestinationUnreachableOfHostUnknown = 0x0307,
+
+        /// <summary>
+        /// Source host isolated
+        /// </summary>
+        DestinationUnreachableOfSourceHostIsolated = 0x0308,
+
+        /// <summary>
+        /// Network administratively prohibited
+        /// </summary>
+        DestinationUnreachableOfNetworkAdministrativelyProhibited  = 0x0309,
+
+        /// <summary>
+        /// Host administratively prohibited 
+        /// </summary>
+        DestinationUnreachableOfHostAdministrativelyProhibited = 0x030A,
+
+        /// <summary>
+        /// Network unreachable for ToS
+        /// </summary>
+        DestinationUnreachableOfNetworkTos = 0x030B,
+
+        /// <summary>
+        /// Host unreachable for ToS
+        /// </summary>
+        DestinationUnreachableOfHostTos = 0x030C,
+
+        /// <summary>
+        /// Communication administratively prohibited
+        /// </summary>
+        DestinationUnreachableOfCommunicationAdministrativelyProhibited = 0x030D,
+
+        /// <summary>
+        /// Host Precedence Violation
+        /// </summary>
+        DestinationUnreachableOfHostPrecedenceViolation = 0x030E,
+
+        /// <summary>
+        /// Precedence cutoff in effect 
+        /// </summary>
+        DestinationUnreachableOfPrecedenceCutoffInEffect = 0x030F,
+
+        #endregion
     }
 }
--- 2-test/networking.model.tests/InternetTests/ICMPv4PacketTests/ICMPv4Packet_Property_TypeCode_Test.cs
+++ 2-test/networking.model.tests/InternetTests/ICMPv4PacketTests/ICMPv4Packet_Property_TypeCode_Test.cs
@@ -11,7 +11,23 @@ namespace Networking.Model.Tests.InternetTests.ICMPv4PacketTests
         public static List<Object[]> Data => new List<Object[]>
         {
             new Object[] { new Byte[]{ 0x08, 0x00 }, ICMPv4TypeCode.EchoRequest },
-            new Object[] { new Byte[]{ 0x00, 0x00 }, ICMPv4TypeCode.EchoResponse }
+            new Object[] { new Byte[]{ 0x00, 0x00 }, ICMPv4TypeCode.EchoResponse },
+            new Object[] { new Byte[]{ 0x03, 0x00 }, ICMPv4TypeCode.DestinationUnreachableOfNetwork },
+            new Object[] { new Byte[]{ 0x03, 0x01 }, ICMPv4TypeCode.DestinationUnreachableOfHost },
+            new Object[] { new Byte[]{ 0x03, 0x02 }, ICMPv4TypeCode.DestinationUnreachableOfProtocol },
+            new Object[] { new Byte[]{ 0x03, 0x03 }, ICMPv4TypeCode.DestinationUnreachableOfPort },
+            new Object[] { new Byte[]{ 0x03, 0x04 }, ICMPv4TypeCode.DestinationUnreachableOfFragmentationRequired },
+            new Object[] { new Byte[]{ 0x03, 0x05 }, ICMPv4TypeCode.DestinationUnreachableOfSourceRouteFailed },
+            new Object[] { new Byte[]{ 0x03, 0x06 }, ICMPv4TypeCode.DestinationUnreachableOfNetworkUnknown },
+            new Object[] { new Byte[]{ 0x03, 0x07 }, ICMPv4TypeCode.DestinationUnreachableOfHostUnknown },
+            new Object[] { new Byte[]{ 0x03, 0x08 }, ICMPv4TypeCode.DestinationUnreachableOfSourceHostIsolated },
+            new Object[] { new Byte[]{ 0x03, 0x09 }, ICMPv4TypeCode.DestinationUnreachableOfNetworkAdministrativelyProhibited },
+            new Object[] { new Byte[]{ 0x03, 0x0A }, ICMPv4TypeCode.DestinationUnreachableOfHostAdministrativelyProhibited },
+            new Object[] { new Byte[]{ 0x03, 0x0B }, ICMPv4TypeCode.DestinationUnreachableOfNetworkTos },
+            new Object[] { new Byte[]{ 0x03, 0x0C }, ICMPv4TypeCode.DestinationUnreachableOfHostTos },
+            new Object[] { new Byte[]{ 0x03, 0x0D }, ICMPv4TypeCode.DestinationUnreachableOfCommunicationAdministrativelyProhibited },
+            new Object[] { new Byte[]{ 0x03, 0x0E }, ICMPv4TypeCode.DestinationUnreachableOfHostPrecedenceViolation },
+            new Object[] { new Byte[]{ 0x03, 0x0F }, ICMPv4TypeCode.DestinationUnreachableOfPrecedenceCutoffInEffect },
         };
 
         [Theory]
@@ -29,9 +45,8 @@ namespace Networking.Model.Tests.InternetTests.ICMPv4PacketTests
         }
 
         [Theory]
-        [InlineData(ICMPv4TypeCode.EchoRequest)]
-        [InlineData(ICMPv4TypeCode.EchoResponse)]
-        public void Set(ICMPv4TypeCode input)
+        [MemberData(nameof(Data))]
+        public void Set(Byte[] expected, ICMPv4TypeCode input)
         {
             var icmpv4Packet = new ICMPv4Packet
             {
@@ -39,7 +54,7 @@ namespace Networking.Model.Tests.InternetTests.ICMPv4PacketTests
             };
 
             icmpv4Packet.TypeCode = input;
-
+            icmpv4Packet[0, 2].ToArray().Should().Equal(expected);
             icmpv4Packet.TypeCode.Should().Be(input);
         }
     }

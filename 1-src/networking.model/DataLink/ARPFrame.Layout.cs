f11b99458345d05b640672b5e2cc61498e6a0569
lnh <lnhcode@outlook.com>
2019-02-17 13:50:53 +0800

    Update ARPFrame.Layout.cs

--- 1-src/networking.model/DataLink/ARPFrame.Layout.cs
+++ 1-src/networking.model/DataLink/ARPFrame.Layout.cs
@@ -1,4 +1,5 @@
 using System;
+using Networking.Model.Internet;
 
 namespace Networking.Model.DataLink
 {
@@ -31,95 +32,104 @@ namespace Networking.Model.DataLink
         /// </summary>
         public class Layout
         {
+
             /// <summary>
             /// 硬件类型-起始位置=0
             /// </summary>
-            public static readonly Int32 HardwareTypeBegin = 0;
+            public const Int32 HardwareTypeBegin = 0;
 
             /// <summary>
-            /// 硬件类型-结束位置=2
+            /// 硬件类型-结束位置
             /// </summary>
-            public static readonly Int32 HardwareTypeEnd = 2;
+            public const Int32 HardwareTypeEnd = HardwareTypeBegin + 2;
+
 
             /// <summary>
             /// 协议类型-起始位置=2
             /// </summary>
-            public static readonly Int32 ProtocolTypeBegin = HardwareTypeEnd;
+            public const Int32 ProtocolTypeBegin = HardwareTypeEnd;
 
             /// <summary>
             /// 协议类型-结束位置=4
             /// </summary>
-            public static readonly Int32 ProtocolTypeEnd = ProtocolTypeBegin + 2;
+            public const Int32 ProtocolTypeEnd = ProtocolTypeBegin + 2;
+
 
             /// <summary>
             /// 硬件地址长度-起始位置=4
             /// </summary>
-            public static readonly Int32 HardwareAddressLengthBegin = ProtocolTypeEnd;
+            public const Int32 HardwareAddressLengthBegin = ProtocolTypeEnd;
 
             /// <summary>
             /// 硬件地址长度-结束位置=5
             /// </summary>
-            public static readonly Int32 HardwareAddressLengthEnd = HardwareAddressLengthBegin + 1;
+            public const Int32 HardwareAddressLengthEnd = HardwareAddressLengthBegin + 1;
+
 
             /// <summary>
             /// 协议地址长度-起始位置=5
             /// </summary>
-            public static readonly Int32 ProtocolAddressLengthBegin = HardwareAddressLengthEnd;
+            public const Int32 ProtocolAddressLengthBegin = HardwareAddressLengthEnd;
 
             /// <summary>
             /// 协议地址长度-结束位置=6
             /// </summary>
-            public static readonly Int32 ProtocolAddressLengthEnd = ProtocolAddressLengthBegin + 1;
+            public const Int32 ProtocolAddressLengthEnd = ProtocolAddressLengthBegin + 1;
+
 
             /// <summary>
             /// 操作码-起始位置=6
             /// </summary>
-            public static readonly Int32 OperationCodeBegin = ProtocolAddressLengthEnd;
+            public const Int32 OperationCodeBegin = ProtocolAddressLengthEnd;
 
             /// <summary>
             /// 操作码-结束位置=8
             /// </summary>
-            public static readonly Int32 OperationCodeEnd = OperationCodeBegin + 2;
+            public const Int32 OperationCodeEnd = OperationCodeBegin + 2;
+
 
             /// <summary>
             /// 发送者硬件地址-起始位置=8
             /// </summary>
-            public static readonly Int32 SenderHardwareAddressBegin = OperationCodeEnd;
+            public const Int32 SenderHardwareAddressBegin = OperationCodeEnd;
 
             /// <summary>
             /// 发送者硬件地址-结束位置=14
             /// </summary>
-            public static readonly Int32 SenderHardwareAddressEnd = SenderHardwareAddressBegin + 6;
+            public const Int32 SenderHardwareAddressEnd = SenderHardwareAddressBegin + MACAddress.Layout.Length;
+
 
             /// <summary>
             /// 发送者协议地址-起始位置=14
             /// </summary>
-            public static readonly Int32 SenderProtocolAddressBegin = SenderHardwareAddressEnd;
+            public const Int32 SenderProtocolAddressBegin = SenderHardwareAddressEnd;
 
             /// <summary>
             /// 发送者协议地址-结束位置=18
             /// </summary>
-            public static readonly Int32 SenderProtocolAddressEnd = SenderProtocolAddressBegin + 4;
+            public const Int32 SenderProtocolAddressEnd = SenderProtocolAddressBegin + IPAddress.Layout.V4Length;
+
 
             /// <summary>
             /// 接收者硬件地址-起始位置=18
             /// </summary>
-            public static readonly Int32 TargetHardwareAddressBegin = SenderProtocolAddressEnd;
+            public const Int32 TargetHardwareAddressBegin = SenderProtocolAddressEnd;
 
             /// <summary>
             /// 接收者硬件地址-结束位置=24
             /// </summary>
-            public static readonly Int32 TargetHardwareAddressEnd = TargetHardwareAddressBegin + 6;
+            public const Int32 TargetHardwareAddressEnd = TargetHardwareAddressBegin + +MACAddress.Layout.Length;
+
 
             /// <summary>
             /// 接收者协议地址-起始位置=24
             /// </summary>
-            public static readonly Int32 TargetProtocolAddressBegin = TargetHardwareAddressEnd;
+            public const Int32 TargetProtocolAddressBegin = TargetHardwareAddressEnd;
 
             /// <summary>
             /// 接收者协议地址-结束位置=28
             /// </summary>
-            public static readonly Int32 TargetProtocolAddressEnd = TargetProtocolAddressBegin + 4;
+            public const Int32 TargetProtocolAddressEnd = TargetProtocolAddressBegin + +IPAddress.Layout.V4Length;
 
         }
     }

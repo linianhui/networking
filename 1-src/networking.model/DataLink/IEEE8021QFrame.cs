629cfd8214b33a61d3f4f574494b0eefba9d7dcc
lnh <lnhcode@outlook.com>
2019-02-27 21:28:53 +0800

    Add IEEE8021QFrame.Layout

---
+++ 1-src/networking.model/DataLink/IEEE8021QFrame.cs
@@ -0,0 +1,11 @@
+namespace Networking.Model.DataLink
+{
+    /// <summary>
+    /// VLAN : IEEE_802.1Q
+    /// <see href="https://en.wikipedia.org/wiki/IEEE_802.1Q"/>
+    /// </summary>
+    public partial class IEEE8021QFrame : Octets
+    {
+
+    }
+}

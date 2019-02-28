5684ca3ca581f3aa711a8caedfac7ff0aac61abe
lnh <lnhcode@outlook.com>
2019-02-28 21:24:31 +0800

    Add VXLANFrame.Layout

---
+++ 1-src/networking.model/DataLink/VXLANFrame.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Networking.Model.DataLink
+{
+    /// <summary>
+    /// VXLAN : RFC7348
+    /// <see href="https://en.wikipedia.org/wiki/Virtual_Extensible_LAN"/>
+    /// <see href="https://tools.ietf.org/html/rfc7348"/>
+    /// </summary>
+    public partial class VXLANFrame : Octets
+    {
+        
+    }
+}

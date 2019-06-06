78af48d7f7501458b50e326e139d9f07369cd7e9
lnh <lnhcode@outlook.com>
2019-06-07 01:19:11 +0800

    Revert "Print env"
    This reverts commit 3fc875a923d159d48ab96d734ad4b1ba7ed2d16d.

--- cake.ps1
+++ cake.ps1
@@ -5,7 +5,6 @@
 $ENV:GIT_COMMIT_SHA = git rev-parse --short HEAD
 Write-Host "GIT_COMMIT_SHA: $ENV:GIT_COMMIT_SHA" -ForegroundColor GREEN
 
-Get-ChildItem ENV:
 # Install cake.tool
 dotnet tool install --global cake.tool --version $CAKE_VERSION
 
--- cake.sh
+++ cake.sh
@@ -7,8 +7,6 @@ CAKE_VERSION='0.33.0'
 export GIT_COMMIT_SHA=$(git rev-parse --short HEAD)
 echo "\033[32mGIT_COMMIT_SHA: $GIT_COMMIT_SHA"
 
-env
-
 # Install  cake.tool
 dotnet tool install --global cake.tool --version $CAKE_VERSION
 export PATH="$PATH:$HOME/.dotnet/tools"

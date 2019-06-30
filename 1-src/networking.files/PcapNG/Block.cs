591f8ceac87e7d251e320dcc064c66fc69ad05c8
lnh <lnhcode@outlook.com>
2019-06-30 21:10:42 +0800

    Add Block.Layout

---
+++ 1-src/networking.files/PcapNG/Block.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Networking.Files.PcapNG
+{
+    /// <summary>
+    /// General Block
+    /// <see href="https://pcapng.github.io/pcapng/#rfc.section.3.1"/>
+    /// </summary>
+    public partial class Block : Octets
+    {
+
+    }
+}
---
+++ 2-test/networking.files.tests/PcapNGTests/BlockTests/Block_Layout_Test.cs
@@ -0,0 +1,22 @@
+using FluentAssertions;
+using Networking.Files.PcapNG;
+using Xunit;
+
+namespace Networking.Files.Tests.PcapNGTests.BlockTests
+{
+    public class Block_Layout_Test
+    {
+        [Fact]
+        public void Layout()
+        {
+            Block.Layout.TypeBegin.Should().Be(0);
+            Block.Layout.TypeEnd.Should().Be(4);
+
+            Block.Layout.TotalLengthBegin.Should().Be(4);
+            Block.Layout.TotalLengthEnd.Should().Be(8);
+
+
+            Block.Layout.BodyBegin.Should().Be(8);
+        }
+    }
+}

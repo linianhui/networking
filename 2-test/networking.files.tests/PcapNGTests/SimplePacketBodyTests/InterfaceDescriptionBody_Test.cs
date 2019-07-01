da6cacfee460ff9cf9f7d5d7192080fe37a5c68c
lnh <lnhcode@outlook.com>
2019-07-01 21:59:40 +0800

    Add SimplePacketBody

---
+++ 2-test/networking.files.tests/PcapNGTests/SimplePacketBodyTests/InterfaceDescriptionBody_Test.cs
@@ -0,0 +1,25 @@
+using System;
+using FluentAssertions;
+using Networking.Files.PcapNG;
+using Xunit;
+
+namespace Networking.Files.Tests.PcapNGTests.SimplePacketBodyTests
+{
+    public class SimplePacketBody_Test
+    {
+        [Fact]
+        public void Get()
+        {
+            var simplePacketBody = new SimplePacketBody
+            {
+                Bytes = new Byte[] {
+                    0x00,0x00,0x00,0x80,
+                    0x12,0x34,0x56,0x78
+                }
+            };
+
+            simplePacketBody.OriginalLength.Should().Be(0x80);
+            simplePacketBody.Data.ToArray().Should().Equal(0x12, 0x34, 0x56, 0x78);
+        }
+    }
+}

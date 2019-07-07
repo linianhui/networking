41a43679576c08aa888e36f68f2463941f0b57ce
lnh <lnhcode@outlook.com>
2019-07-07 21:55:05 +0800

    Refactor PcapFileHeader

--- 1-src/networking.files/Pcap/PcapFileHeader.cs
+++ 1-src/networking.files/Pcap/PcapFileHeader.cs
@@ -9,11 +9,6 @@ namespace Networking.Files.Pcap
     public partial class PcapFileHeader : Octets
     {
         /// <summary>
-        /// 是否是纳秒
-        /// </summary>
-        public Boolean TimestampMicrosecondPartIsNanosecond { get; set; }
-
-        /// <summary>
         /// 构造函数
         /// </summary>
         /// <param name="headerBytes">首部Byte[]</param>
@@ -21,46 +16,61 @@ namespace Networking.Files.Pcap
         {
             Bytes = headerBytes;
             IsLittleEndian = base.GetByte(0) != 0xA1;
-            TimestampMicrosecondPartIsNanosecond = ComputeTimestampMicrosecondPartIsNanosecond();
-            MagicNumber = GetUInt32(Layout.MagicNumberBegin);
-            MajorVersion = GetUInt16(Layout.MajorVersionBegin);
-            MinorVersion = GetUInt16(Layout.MinorVersionBegin);
-            MaxCapturedLength = GetUInt32(Layout.MaxCapturedLengthBegin);
-            Type = (DataLinkType)GetUInt32(Layout.DataLinkTypeBegin);
+        }
+
+        /// <summary>
+        /// 数据链路类型
+        /// </summary>
+        public DataLinkType Type
+        {
+            get { return (DataLinkType)GetUInt32(Layout.DataLinkTypeBegin); }
         }
 
         /// <summary>
         /// Magic Number
         /// </summary>
-        public UInt32 MagicNumber { get; }
+        public UInt32 MagicNumber
+        {
+            get { return GetUInt32(Layout.MagicNumberBegin); }
+        }
 
         /// <summary>
         /// Major Version
         /// </summary>
-        public UInt16 MajorVersion { get; }
+        public UInt16 MajorVersion
+        {
+            get { return GetUInt16(Layout.MajorVersionBegin); }
+        }
 
         /// <summary>
         /// Minor Version
         /// </summary>
-        public UInt16 MinorVersion { get; }
+        public UInt16 MinorVersion
+        {
+            get { return GetUInt16(Layout.MinorVersionBegin); }
+        }
 
         /// <summary>
         /// 数据包最大长度
         /// </summary>
-        public UInt32 MaxCapturedLength { get; }
+        public UInt32 MaxCapturedLength
+        {
+            get { return GetUInt32(Layout.MaxCapturedLengthBegin); }
+        }
 
         /// <summary>
-        /// 数据链路类型
+        /// 是否是纳秒
         /// </summary>
-        public DataLinkType Type { get; }
-
-        private Boolean ComputeTimestampMicrosecondPartIsNanosecond()
+        public Boolean TimestampMicrosecondPartIsNanosecond
         {
-            if (IsLittleEndian)
+            get
             {
-                return base.GetByte(0) == 0x4D;
+                if (IsLittleEndian)
+                {
+                    return base.GetByte(0) == 0x4D;
+                }
+                return base.GetByte(3) == 0x4D;
             }
-            return base.GetByte(3) == 0x4D;
         }
     }
 }

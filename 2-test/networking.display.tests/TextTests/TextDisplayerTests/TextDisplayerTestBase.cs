3dffc74b40ecb1fd4f2f9034564814fe7f8ec48b
lnh <lnhcode@outlook.com>
2019-07-20 17:54:07 +0800

    Update TextDisplayerTestBase.cs

--- 2-test/networking.display.tests/TextTests/TextDisplayerTests/TextDisplayerTestBase.cs
+++ 2-test/networking.display.tests/TextTests/TextDisplayerTests/TextDisplayerTestBase.cs
@@ -1,14 +1,10 @@
-using System;
 using System.IO;
 using Moq;
-using Networking.Display.Text;
 
 namespace Networking.Display.Tests.TextTests.TextDisplayerTests
 {
     public class TextDisplayerTestBase
     {
-        protected
-
         internal (IDisplayer displayer, Mock<TextWriter> mockTextWriter) BuildTextDisplayer()
         {
             var mockTextWriter = new Mock<TextWriter>();

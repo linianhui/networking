4d2d91aea1b9c8e52f3e07be8ccd1fa749b846c0
lnh <lnhcode@outlook.com>
2019-10-08 13:26:35 +0800

    Fix cake.sh

--- cake.sh
+++ cake.sh
@@ -3,16 +3,14 @@
 set -x
 
 SCRIPT='0-build/build.cake'
-CAKE_VERSION='0.35.0'
 
 # git commit sha
 if command -v git >/dev/null 2>&1; then 
   GIT_COMMIT_SHA=$(git rev-parse --short HEAD)
 fi
 
-# Install  cake.tool
-dotnet tool install --global --version $CAKE_VERSION cake.tool
-export PATH="$PATH:$HOME/.dotnet/tools"
+# Restore cake.tool
+dotnet tool restore
 
 # Start Cake
 CAKE_ARGS="$SCRIPT --verbosity=diagnostic -git-commit-sha=$GIT_COMMIT_SHA"

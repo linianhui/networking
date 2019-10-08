a95266a83b63b4ebaad1a3816b501fac1747fd4c
lnhcode <lnhcode@outlook.com>
2019-10-08 13:56:29 +0800

    Update cake.ps1

--- cake.ps1
+++ cake.ps1
@@ -1,17 +1,13 @@
 [string]$SCRIPT = '0-build/build.cake'
 
-# dotnet info
 dotnet --info
 
-# git commit sha
+dotnet tool restore
+
 if (Get-Command git -ErrorAction SilentlyContinue){
     [string]$GIT_COMMIT_SHA = git rev-parse --short HEAD
 }
 
-# Restore cake.tool
-dotnet tool restore
-
-# Start Cake
 [string]$CAKE_ARGS = "--verbosity=diagnostic"
 [string]$CAKE_GIT_ARGS ="-git-commit-sha=$GIT_COMMIT_SHA"
 

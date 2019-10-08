f2e30362f41aa63a998f0fc45285b42d17808d51
lnh <lnhcode@outlook.com>
2019-10-08 13:26:55 +0800

    Fix cake.ps1

--- cake.ps1
+++ cake.ps1
@@ -1,13 +1,12 @@
 [string]$SCRIPT       = '0-build/build.cake'
-[string]$CAKE_VERSION = '0.35.0'
 
 # git commit sha
 if (Get-Command git -ErrorAction SilentlyContinue){
     [string]$GIT_COMMIT_SHA = git rev-parse --short HEAD
 }
 
-# Install cake.tool
-dotnet tool install --global --version $CAKE_VERSION cake.tool
+# Restore cake.tool
+dotnet tool restore
 
 # Start Cake
 [string]$CAKE_ARGS = "--verbosity=diagnostic"
